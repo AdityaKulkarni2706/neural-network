5cfb9b864842d1caed0f14536b514a7ef8b1a77a
Aditya <noxx.music27@gmail.com>
2025-12-24 22:41:48 +0300

    tensor done

---
+++ build/CMakeFiles/neural_net_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neural_net_lib.dir/src/tensor.cpp.o"
+  "CMakeFiles/neural_net_lib.dir/src/tensor.cpp.o.d"
+  "libneural_net_lib.a"
+  "libneural_net_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/neural_net_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/aditya/neural-net
+# Build directory: /home/aditya/neural-net/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(CoreTests "/home/aditya/neural-net/build/run_tests")
+set_tests_properties(CoreTests PROPERTIES  _BACKTRACE_TRIPLES "/home/aditya/neural-net/CMakeLists.txt;54;add_test;/home/aditya/neural-net/CMakeLists.txt;0;")
